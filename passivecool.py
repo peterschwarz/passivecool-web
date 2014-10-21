9b4b888306f6d6b3d418b1dc4dc3bbe40df0c993
Curtis Thompson <curtis@oddpost.com>
2014-10-20 22:29:36 -0500

    Add mock data feature and fix ui

--- passivecool.py
+++ passivecool.py
@@ -1,3 +1,10 @@
+"""Usage:
+  passivecool.py [--host=0.0.0.0] [--port=5000] [--mock_arduino]
+  passivecool.py -h | --help | --version
+"""
+
+import collections
+from docopt import docopt
 import requests
 import random
 
@@ -8,10 +15,23 @@ from flask.ext import restful
 app = Flask(__name__, static_url_path='/www', static_folder='./static/passivecool/www' )
 api = restful.Api(app)
 
+args = {}
+
+def mock_arduino():
+    import random
+    return {
+            'temperature': random.randint(60, 90),
+            'humidity': random.randint(40, 90) + random.randint(0,99)/100.0,
+            'light_level': random.randint(0,1023),
+            'position': random.randint(0,90)
+    }
 
 class StatusApi(restful.Resource):
     def get(self):
-        json = requests.get('http://passivecool.local/arduino/state').json()
+        if args.get('--mock_arduino'):
+            json = mock_arduino() 
+        else:
+            json = requests.get('http://passivecool.local/arduino/state').json()
         print json
         return json
 
@@ -21,11 +41,12 @@ class StatusApi(restful.Resource):
         return {}
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    return redirect("/www/index.html")
+    return redirect('/www/index.html')
 
 api.add_resource(StatusApi, '/api/status')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    args = docopt(__doc__, version='0.1')
+    app.run(host=args['--host'] or '0.0.0.0', port=int(args['--port']))
--- static/passivecool/www/js/controllers.js
+++ static/passivecool/www/js/controllers.js
@@ -1,41 +1,30 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope, $interval, $http, Status) {
-    $scope.manual_override = false;
-    $scope.position = 0;
+    $scope.manualOverride = {};
+    $scope.manualOverride.isEnabled = false;
+    $scope.manualOverride.position = 0;
+
     var interval_func = function() {
         $http({method: 'GET', url: '/api/status'})
             .success(function(data, status, headers, config){
                 $scope.status = data;
-                if (!$scope.manual_override) {
-                    $scope.position = $scope.status.position;
+                if (!$scope.manualOverride.isEnabled) {
+                    $scope.manualOverride.position = $scope.status.position;
                 }
             });
          };
 
-    $scope.$watch('manual_override', function(newValue, oldValue) {
-        console.log("manual_override");
-        if (newValue) {
-            console.log("manual overrride")
-//            $http({method: 'PUT', url: '/api/status/' + newValue});
-        } else {
-            console.log("auto mode")
+    $scope.positionChanged = function() {
+        if ($scope.manualOverride.isEnabled) {
+            console.log("changing position to " + $scope.manualOverride.position)
+            $http({method: 'PUT', url: '/api/status/' + $scope.manualOverride.position});
         }
-    });
-
-    $scope.$watch('position', function(newValue, oldValue) {
-        console.log("position");
-        if ($scope.manual_override) {
-            console.log("changing position to")
-            $http({method: 'PUT', url: '/api/status/' + newValue});
-        }
-    });
+    };
 
 
- $interval(interval_func, 1000 * 10);
-
-
- $scope.status = Status.get();
+    $interval(interval_func, 1000 * 10);
+    $scope.status = Status.get();
 
 })
 
